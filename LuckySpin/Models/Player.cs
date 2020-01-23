07aa2d4619f66513f6f559af211e6fe0fa3032ce
Gregory Jesionowski <gregory.a.jesionowski@gmail.com>
2020-01-23 13:28:32 -0800

    Pre-branch

--- LuckySpin/Models/Player.cs
+++ LuckySpin/Models/Player.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-//TODO: Add the library for Data Annotations
+using System.ComponentModel.DataAnnotations;
 namespace LuckySpin.Models
 {
     public class Player
@@ -7,9 +7,12 @@ namespace LuckySpin.Models
         //TODO: Include two annotations for FirstName
         // one that makes it Required (no empty strings)
         // and another that sets its Display prompt to "Your First Name"
+        [Required(ErrorMessage ="You must enter your name"), Display(Prompt = "Your name")]
+        
         public string FirstName { get; set; }
         //TODO: Include one annotation for LuckyNumber
         // one that limits its Range between 1 and 9
+        [Required(), Range(1,9)]
         public int LuckyNumber { get; set; }
     }
 }
